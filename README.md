c61370712362695d65d224a98e5e7adf235ff9ad
Kyle Van Bergen <kyleavb@gmail.com>
2018-01-12 11:11:13 -0800

    created game logic

--- README.md
+++
@@ -1,2 +0,0 @@
-# wdi-fundamentals-memorygame
-Learning
--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("Number of Cards In Play: " + cardsInPlay.length);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a Match!");
+	}else{
+		alert("Sorry try again")
+	}
+}
