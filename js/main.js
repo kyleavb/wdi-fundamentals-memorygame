baf054c0f37549b6793e27830ab7b79c05abd48c
Kyle Van Bergen <kyleavb@gmail.com>
2018-01-12 14:14:54 -0800

    created even more game logic unit 10

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
-var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+var cards = [
+{
+	rank: "queen",
+	suit: "heart",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "heart",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 
 var checkForMatch = function(){
-	if(cardsInPlay[0] === cardsInPlay[1]){
+	if(cardsInPlay[0].rank === cardsInPlay[1].rank){
 		alert("You found a Match!");
 	}else{
 		alert("Sorry try again");
@@ -10,7 +32,9 @@ var checkForMatch = function(){
 }
 
 var flipCard = function(cardId){
-	console.log("User flipped "+ cards[cardId]);
+	console.log("User flipped "+ cards[cardId].rank);
+	console.log("Card Suit: "+ cards[cardId].suit);
+	console.log("Image Location: "+ cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId]);
 	if(cardsInPlay.length === 2){
 		checkForMatch();
@@ -18,7 +42,7 @@ var flipCard = function(cardId){
 	
 }
 
-var bob = prompt("Number");
+var bob = prompt("Number ");
 flipCard(bob);
 var bit = prompt("Number 0-3");
 flipCard(bit);
