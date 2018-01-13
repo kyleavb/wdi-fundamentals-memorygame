caed7dd001089691d9dc398805791f847d6a467b
Kyle Van Bergen <kyleavb@gmail.com>
2018-01-12 21:01:53 -0800

    v2.0 game

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var userScore = 0;
+var userAttempts = 3;
 var cardsInPlay = [];
 var cards = [
 {
@@ -22,34 +24,82 @@ var cards = [
 }
 ];
 
-
 var checkForMatch = function(){
-	if(cardsInPlay[0].rank === cardsInPlay[1].rank){
-		alert("You found a Match!");
+	if(userAttempts > 1){
+		if(cardsInPlay[0].rank === cardsInPlay[1].rank){
+			alert("You found a Match!");
+			userScore = userScore + 100;
+			updateHud(false, true);
+		}else{
+			alert("Sorry try again");
+			userAttempts = userAttempts - 1;
+			updateHud(true, false);
+		}
 	}else{
-		alert("Sorry try again");
+			alert("Game Over");
+			document.getElementsByClassName("heart")[0].remove();
 	}
 }
-
 var flipCard = function(){
 	var cardId = this.getAttribute("data-id");
 	this.setAttribute("src", cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId]);
-	
-
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 	}
 }
-
+var createHud = function(){
+	for(i=0;i<userAttempts;i++){
+		lifeIcon = document.createElement("img");
+		lifeIcon.setAttribute("class", "heart");
+		lifeIcon.setAttribute("src", "images/heart.png")
+		document.getElementsByClassName("attempts")[0].appendChild(lifeIcon);
+	}
+	scoreElement = document.createElement("p");
+	scoreElement.setAttribute("class", "currentScore");
+	scoreElement.innerHTML = userScore;
+	document.getElementsByClassName("score")[0].appendChild(scoreElement);
+}
+var updateHud = function(lifeDown, scoreUp){
+	if(lifeDown == true){
+		document.getElementsByClassName("heart")[0].remove();
+	}else if(scoreUp == true){
+		var setScore = document.getElementsByClassName("currentScore")[0].innerHTML = userScore; 
+	}
+	resetBoard();
+}
 var createBoard = function(){
 	for(i=0;i<cards.length;i++){
 		cardElement = document.createElement("img");
 		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("class", "playingCard")
 		cardElement.setAttribute("data-id", i);
 		cardElement.addEventListener("click", flipCard);
 		document.getElementById("game-board").appendChild(cardElement);
 	}
 }
+var resetBoard = function(){
+	for(i=0;i<cards.length;i++){
+		document.getElementsByClassName("playingCard")[i].setAttribute("src", "images/back.png");
+	}
+	shuffle(cards);
+	cardsInPlay = [];
+}
+
+'did not write this function, found online'
+function shuffle (array) {
+  var i = 0
+    , j = 0
+    , temp = null
+
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+}
+
+createHud();
 createBoard();
 
